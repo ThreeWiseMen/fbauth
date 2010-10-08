9867b6fbb18904ed5544d1bd7a92b2d7099496b8
Steven Vetzal <steve@vetzal.com>
2010-10-08 19:48:40 -0400

    adding js auth api

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,6 +1,20 @@
 class AuthController < ApplicationController
 
   def welcome
+    @access_token = parse_cookie(cookies["fbs_114653015261512"])
+  end
+
+private
+
+  def parse_cookie(cookie)
+    unless cookie.nil?
+      parms = {}
+      cookie.split("&").each do |pair|
+        key, value = pair.split("=")
+        parms[key] = value
+      end
+    end
+    parms
   end
 
 end
