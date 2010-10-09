5e518d05ed95ea27dcb755f8217520281535be15
Steven Vetzal <steve@vetzal.com>
2010-10-08 21:34:44 -0400

    adding js auth api

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -5,6 +5,7 @@ class AuthController < ApplicationController
 
     @parms = parse_parms
     @cookie = parse_cookie
+    @user = facebook_user @access_token
   end
 
 private
@@ -32,6 +33,31 @@ private
     parms = parse_parms
     parms = parse_cookie if parms.nil?
     parms['access_token'] unless parms.nil?
+  end
+
+  def facebook_graph(path, access_token = nil, options = {})
+    params = []
+    options.keys.each do |k|
+      params << "#{k}=#{options[k]}" unless options[k].nil?
+    end
+    token = access_token ? CGI::escape(access_token) : nil
+    url = "https://graph.facebook.com/#{path}&access_token=#{token}"
+    url = "#{url}?#{params.join('&')}" unless params.empty?
+    uri = URI.parse(url)
+    http = Net::HTTP.new uri.host, uri.port
+    begin
+      http.use_ssl = (uri.scheme == "https")
+      req = Net::HTTP::Get.new(uri.path)
+      response = http.request(req)
+      json = JSON.parse(response.body)
+    ensure
+      http.finish if http.started?
+    end
+    json
+  end
+
+  def facebook_user(access_token)
+    facebook_graph('me', access_token )
   end
 
 end
