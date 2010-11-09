84b9e25e5a3b1ed49838e546481747915bea9c53
Steven Vetzal <steve@vetzal.com>
2010-11-08 19:01:05 -0500

    making data nill on bad token

--- vendor/plugins/fbauth/app/controllers/facebook_auth_controller.rb
+++ vendor/plugins/fbauth/app/controllers/facebook_auth_controller.rb
@@ -24,7 +24,7 @@ private
       data[:user] = FacebookGraph.call(data[:uid], data[:access_token]) if parms.has_key? 'uid' && parms.has_key? 'access_token'
       if data[:user].has_key? 'error'
         logger.error "Invalid access token, #{data[:user]['error'].inspect}"
-        data = {}
+        data = nil
       end
     end
     data
