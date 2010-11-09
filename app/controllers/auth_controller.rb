723974cdb601e8097d878c1370b159cc111ee798
Steven Vetzal <steve@vetzal.com>
2010-11-08 19:06:37 -0500

    removing dead method

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -3,12 +3,6 @@ class AuthController < ApplicationController
 
   before_filter :setup_facebook_auth
 
-  def welcome
-    @parms = parse_parms
-    @cookie = parse_cookie
-    @user = facebook_user @facebook_auth_data[:access_token]
-  end
-
   def authenticate
   end
 
