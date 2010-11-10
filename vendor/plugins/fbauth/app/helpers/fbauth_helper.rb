d3e704b49850f4439d0b9f5077ed5c466f5a1e5f
Steven Vetzal <steve@vetzal.com>
2010-11-10 14:10:46 -0500

    bit more refactoring, removing more boilerplate

--- vendor/plugins/fbauth/app/helpers/fbauth_helper.rb
+++ vendor/plugins/fbauth/app/helpers/fbauth_helper.rb
@@ -1,9 +1,13 @@
 module FbauthHelper
- def fbauth_javascript options={}
+ def fbauth_login_javascript options={}
   login_el = options[:login] || 'login'
   add_el = options[:add] || 'add'
   ready_el = options[:ready] || 'ready'
 
-  render :partial => '/fbauth/javascript.html.haml', :locals => { :login_el => login_el, :add_el => add_el, :ready_el => ready_el }
+  render :partial => '/fbauth/login.html.haml', :locals => { :login_el => login_el, :add_el => add_el, :ready_el => ready_el }
+ end
+
+ def fbauth_init_javascript
+  render :partial => '/fbauth/init.html.haml'
  end
 end
