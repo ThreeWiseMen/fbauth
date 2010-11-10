c8fcc78e0f80a8e413e3394f96a7180112e3a9d9
Steven Vetzal <steve@vetzal.com>
2010-11-10 13:05:06 -0500

    don't think we actually need fb data on auth page

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -2,7 +2,7 @@ class AuthController < ApplicationController
   include FacebookAuthController
 
   # Do we even need this when using the JS SDK for auth?
-  before_filter :setup_facebook_auth
+  #before_filter :setup_facebook_auth
 
   def authenticate
   end
