e3991dc8ad27606f9e18afd0e64f73c9b7a50787
Steven Vetzal <steve@vetzal.com>
2010-11-10 14:29:38 -0500

    internalizing variable name, hiding behind helper

--- vendor/plugins/fbauth/app/helpers/fbauth_helper.rb
+++ vendor/plugins/fbauth/app/helpers/fbauth_helper.rb
@@ -1,13 +1,17 @@
 module FbauthHelper
- def fbauth_login_javascript options={}
-  login_el = options[:login] || 'login'
-  add_el = options[:add] || 'add'
-  ready_el = options[:ready] || 'ready'
+  def fbauth_login_javascript options={}
+    login_el = options[:login] || 'login'
+    add_el = options[:add] || 'add'
+    ready_el = options[:ready] || 'ready'
 
-  render :partial => '/fbauth/login.html.haml', :locals => { :login_el => login_el, :add_el => add_el, :ready_el => ready_el }
- end
+    render :partial => '/fbauth/login.html.haml', :locals => { :login_el => login_el, :add_el => add_el, :ready_el => ready_el }
+  end
 
- def fbauth_init_javascript
-  render :partial => '/fbauth/init.html.haml'
- end
+  def fbauth_init_javascript
+    render :partial => '/fbauth/init.html.haml'
+  end
+
+  def fbauth
+    @facebook_auth
+  end
 end
