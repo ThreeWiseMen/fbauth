44c2b7bf3d909a2aad2de305aef9a7bd119da85a
Steven Vetzal <steve@vetzal.com>
2011-03-03 12:15:24 -0500

    changing brackets

--- spec/facebook_decoder_spec.rb
+++ spec/facebook_decoder_spec.rb
@@ -2,7 +2,7 @@ require File.join(File.dirname(__FILE__), "spec_helper.rb")
 
 describe FacebookDecoder, "decode" do
   context "data 1" do
-    before(:each) do
+    before :each do
       @data = "GXXAcWOgOgIHPwHo0vVXCzdfCJ_Dmln3sDuRsZ2oKNo.eyJhbGdvcml0aG0iOiJITUFDLVNIQTI1NiIsImV4cGlyZXMiOjEyOTY2NjYwMDAsImlzc3VlZF9hdCI6MTI5NjY2MTgzNSwib2F1dGhfdG9rZW4iOiIxMTg4OTE2NDgxMjM5MzR8Mi5NR0ZDRFBEb0FZb2ZHa2NpSnZCeHRRX18uMzYwMC4xMjk2NjY2MDAwLTg0OTM5NTIxNnxaLVg3NlVza1MtUUlfN3VEalNKR1FOZ0JMelEiLCJ1c2VyIjp7ImxvY2FsZSI6ImVuX1VTIiwiY291bnRyeSI6ImNhIn0sInVzZXJfaWQiOiI4NDkzOTUyMTYifQ"
     end
 
@@ -15,7 +15,7 @@ describe FacebookDecoder, "decode" do
   end
 
   context "data 2" do
-    before(:each) do
+    before :each do
       @data = "rIDyCvtufQMlamuK3Nx_n54BcGvWrAiHXyOGvDz48ww.eyJhbGdvcml0aG0iOiJITUFDLVNIQTI1NiIsImV4cGlyZXMiOjEyOTY2NzY4MDAsImlzc3VlZF9hdCI6MTI5NjY3MTM5OSwib2F1dGhfdG9rZW4iOiIxMTg4OTE2NDgxMjM5MzR8Mi55eU4wa1RrQXJrYUpwdlJZSWpfRWZBX18uMzYwMC4xMjk2Njc2ODAwLTU4MDAxNjExfHFJbWlOVXMtQVdLdGhDZWpOaHEzby1EZXdvayIsInVzZXIiOnsibG9jYWxlIjoiZW5fVVMiLCJjb3VudHJ5IjoiY2EifSwidXNlcl9pZCI6IjU4MDAxNjExIn0"
     end
 
@@ -28,7 +28,7 @@ describe FacebookDecoder, "decode" do
   end
 
   context "invalid data" do
-    before(:each) do
+    before :each do
       @data = "gibberish"
     end
 
