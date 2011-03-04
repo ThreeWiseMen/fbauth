4b13db6188d1c31aba4deaf54b3eb9b369d605d6
Steven Vetzal <steve@vetzal.com>
2011-03-03 19:28:36 -0500

    implementing caching

--- lib/facebook_http.rb
+++ lib/facebook_http.rb
@@ -15,19 +15,24 @@ module FacebookHttp
 
   def get(url, params = {})
     uri = URI.parse(build_get_url(url, params))
-    http = Net::HTTP.new uri.host, uri.port
-    begin
-      http.use_ssl = (uri.scheme == "https")
-      req = Net::HTTP::Get.new(uri.request_uri)
-      response = http.request(req)
-      raise "Facebook error response #{response.code} - #{response.body}" unless response.code == '200'
+
+    json = Rails.cache.read(uri.to_s)
+    if json.nil?
+      http = Net::HTTP.new uri.host, uri.port
       begin
-        json = JSON.parse(response.body)
-      rescue => e
-        raise "Error parsing facebook response: #{response.body}"
+        http.use_ssl = (uri.scheme == "https")
+        req = Net::HTTP::Get.new(uri.request_uri)
+        response = http.request(req)
+        raise "Facebook error response #{response.code} - #{response.body}" unless response.code == '200'
+        begin
+          json = JSON.parse(response.body)
+        rescue => e
+          raise "Error parsing facebook response: #{response.body}"
+        end
+      ensure
+        http.finish if http.started?
       end
-    ensure
-      http.finish if http.started?
+      Rails.cache.write(uri.to_s, json, expires_in => 300) unless json.nil?
     end
     json
   end
