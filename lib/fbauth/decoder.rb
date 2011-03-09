d6e5f29de6a74b0f54d0f4ba72130db6198d2ad0
Steven Vetzal <steve@vetzal.com>
2011-03-09 09:02:31 -0500

    restoring previous exception message for consistency

--- lib/fbauth/decoder.rb
+++ lib/fbauth/decoder.rb
@@ -8,7 +8,11 @@ class FacebookDecoder
       sig, b64udata = data.split('.')
       unless b64udata.nil?
         json = b64udata.tr('-_', '+/').unpack('m')[0]
-        parms = JSON.parse(balance(json))
+        begin
+          parms = JSON.parse(balance(json))
+        rescue => e
+          raise "Unable to parse json structure - '#{json}'"
+        end
       end
     end
     parms
